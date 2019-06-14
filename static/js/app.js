e4525a89fd03fd241820322ef708876a5cca557e
Lei <koudash@gmail.com>
2019-06-14 00:11:16 -0500

    init .html, .css, and .js files

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+// // Reading data
+// d3.csv("../data/xxx.csv").then((data) => {
+// });
+
+
+// Leaflet basic layer
+let myMap = L.map("map-first", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
