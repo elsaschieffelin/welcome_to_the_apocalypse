e5b6689396288883c2fceeb8169c136ca7418e3d
Elsa Schieffelin <ems18@rice.edu>
2019-06-15 12:43:34 -0500

    data->flask

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 # flask tools
+import json
+import pandas as pd
+from flask import jsonify
 from flask import Flask, render_template
 
 # Create app
 app = Flask(__name__)
+
 
 # Home route
 @app.route('/')
 def index():
     '''Return the homepage.'''
     return render_template('index.html')
+
+#data route
+@app.route('/data')
+def read():
+   data = pd.read_csv('data/master_chart.csv')
+   json_data = json.loads(data.to_json(orient='records'))
+   return jsonify(json_data)
+
 
 # Scripts execution
 if __name__ == "__main__":
