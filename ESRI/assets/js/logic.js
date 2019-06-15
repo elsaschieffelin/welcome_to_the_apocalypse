338c0f923e01d43c9230439b5badedfd77ca9262
Andrea D. Cicero <44382243+kyanite5@users.noreply.github.com>
2019-06-15 12:39:27 -0500

    Add files via upload

--- ESRI/assets/js/logic.js
+++ ESRI/assets/js/logic.js
@@ -1,55 +1,59 @@
-var myMap = L.map('map').setView([48.10, -100.10], 3);
+function createMap() {
 
-L.esri.tiledMapLayer({
-  url: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_basemap_ncei/MapServer?f=pjson&cacheKey=b92695a3fb65e691",
-  maxZoom: 15
-  }).addTo(myMap);
+  const gebco_color = L.esri.tiledMapLayer({
+    url: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_basemap_ncei/MapServer?f=pjson&cacheKey=b92695a3fb65e691",
+    maxZoom: 15
+  });
 
+  const gebco_greyscale = L.esri.tiledMapLayer({
+    url: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_grayscale_basemap_ncei/MapServer?f=pjson&cacheKey=aa7b7421bc725fd9",
+    maxZoom: 15
+  });
 
+  const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: API_KEY
+    });
 
-//
-// // Define basemaps layers: street, dark, satellite
-//   const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.streets",
-//     accessToken: API_KEY
-//   });
-//
-//   const darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.dark",
-//     accessToken: API_KEY
-//   });
-//
-//   const satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.satellite",
-//     accessToken: API_KEY
-//   });
-//
-//   const gebco = L.esri.tiledMapLayer({
-//     url: "https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_basemap_ncei/MapServer/0",
-//   });
-//
-//   // Define a baseMaps object to hold our base layers
-//   const baseMaps = {
-//     "Street Map": streetmap,
-//     "Dark Map": darkmap,
-//     "Satellite Map": satellitemap,
-//     "NOAA Map": gebco
-//   };
-//
-//     // Create overlay object to hold our overlay layer
-//     // const overlayMaps = {
-//     //   Earthquakes: earthquakes,
-//     //   Plates: plates
-//     // };
-//     // Create our map, giving it the streetmap, darkmap, satellite, earthquakes and plates layers to display on load
-//   const myMap = L.map("map", {
-//     center: [48.10, -100.10],
-//     zoom: 3,
-//     layers: [gebco, streetmap, darkmap, satellitemap]
-//   });
+  const darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  const satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  const baseMaps = {
+    Street: streetmap,
+    Dark: darkmap,
+    Satellite: satellitemap,
+    NOAAcolor: gebco_color,
+    NOAAgrey: gebco_greyscale
+  };
+
+  // const overlayMaps = {
+  //   "NA": NA
+  // };
+
+  const myMap = L.map("map", {
+    center: [48.10, -100.10],
+    zoom: 3,
+    layers: [gebco_color]
+  });
+
+  // L.control.layers(baseMaps {
+  //   collapsed: false
+  // }).addTo(myMap);
+  L.control.layers(baseMaps, null, {collapsed:false} ).addTo(myMap);
+}
+
+createMap();
