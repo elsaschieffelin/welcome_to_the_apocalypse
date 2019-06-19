60243c69280d96a4d0061d673d05834fcadefe7d
Lei <koudash@gmail.com>
2019-06-19 15:52:12 -0500

    add event listeners("mouseover" and "mouseout") for "chart-death"

--- static/js/deathByDisasters.js
+++ static/js/deathByDisasters.js
@@ -1,42 +1,89 @@
-// Read death by disasters data
+// Read death by disasters data         <-- the data .csv file has to be inside the static folder as flask is used
 d3.csv("static/death_by_disasters.csv").then((data) => {
-   
-    // Grap all column names from data
-    let colNames = Object.keys(data[0]);
-    // Pop out the first column as x axis of the chart, the remaining columns are considered as y axes
-    let xAxisVar = colNames.shift();
-       
-    // Data array for the Plotly bar chart
-    let dataArr = [];
 
-    // Array for "x" in trace for the Plotly bar chart
-    let xArr = data.map((d) => d[xAxisVar]); 
+    // PLot the death chart
+    deathByDisastersPlot();
 
-    // Update data array with each column setting as y axis
-    colNames.forEach((col) => {
+    // Event listener to update and re-plot the chart in "col-12" when mouse is moved over the chart
+    d3.select("#chart-death").on("mouseover", function() {
+
+        // Change "col-6" to "col-12" in "class" when activated
+        d3.select(this)
+            .attr("class", "col-12 mt-5")
+            .attr("id", "chart-death");
+
+        // Re-plot the chart
+        deathByDisastersPlot();
+
+    }),
+
+    // Event listener to reset the chart in "col-6" when mouse is moved out of the chart
+    d3.select("#chart-death").on("mouseout", function() {
+
+        // Change "col-12" to "col-6" in "class" when activated        
+        d3.select(this)
+            .attr("class", "col-6 mt-5")
+            .attr("id", "chart-death");
         
-        let yArr = [];
+        // Re-plot the chart
+        deathByDisastersPlot();
 
-        data.forEach((d) => {
-            yArr.push(d[col]);
-        });
+    });
 
-        trace = {
-            x: xArr,
-            y: yArr,
-            name: col,
-            type: "bar"
+    /**
+     * Retrieve data of deaths caused by each disaster as stacked y values, assign year of disaster as x-axis, and plot the chart at "div" element with an id of "chart-death"
+     */
+    function deathByDisastersPlot() {
+        // Grap all column names from data
+        let colNames = Object.keys(data[0]);
+        // Pop out the first column as x axis of the chart, the remaining columns are considered as y axes
+        let xAxisVar = colNames.shift();
+        
+        // Data array for the Plotly bar chart
+        let dataArr = [];
+
+        // Array for "x" in trace for the Plotly bar chart
+        let xArr = data.map((d) => d[xAxisVar]); 
+
+        // Update data array with each column setting as y axis
+        colNames.forEach((col) => {
+            
+            let yArr = [];
+
+            data.forEach((d) => {
+                yArr.push(d[col]);
+            });
+
+            trace = {
+                x: xArr,
+                y: yArr,
+                name: col,
+                type: "bar"
+            };
+
+            dataArr.push(trace);
+
+        });         
+
+        // Set the layout of Plotly bar chart as "stack"
+        let layout = {
+            barmode: "stack",
+            xaxis: {
+                tickangle: -45
+            },
+            title: {
+                text: "Death Chart Title???",
+                font: {
+                    family: "Cabin Sketch",
+                    size: 30
+                }
+            }
         };
 
-        dataArr.push(trace);
-
-    });         
-
-    // Set the layout of Plotly bar chart as "stack"
-    let layout = {barmode: "stack"};
-
-    // Plot the chart
-    Plotly.newPlot("chart-death", dataArr, layout);
+        // Plot the chart
+        Plotly.newPlot("chart-death", dataArr, layout, {responsive: true});
+    
+    // End of function
+    }
 
 });
-
